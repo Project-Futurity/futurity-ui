048f886c01a8ddf2ee56265283aa799c16288ce0
ThePersonThat <gaudabrus@inbox.ru>
2021-10-17 17:19:34 +0300

    Added header to the home page

--- futurity/src/app/app.module.ts
+++ futurity/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {AppRoutingModule} from "./app-routing/app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
