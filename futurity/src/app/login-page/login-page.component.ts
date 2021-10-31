fca1c5bcf41bb62ce542fc5c3acd56cf37b0811f
ThePersonThat <gaudabrus@inbox.ru>
2021-10-31 18:13:04 +0200

    Added logic to the login page: 	- Validation the fields 	- Showing/Hiding password 	- Showing the errors on the error response

--- futurity/src/app/login-page/login-page.component.ts
+++ futurity/src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  showPassword = false;
+  disableLoginButton = false;
+  @ViewChild('password') input: ElementRef;
+  loginForm: FormGroup;
+  loginError: string = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
   }
 
+  togglePasswordView() {
+    this.showPassword = !this.showPassword;
+    this.input.nativeElement.type = this.showPassword ? "text" : "password";
+  }
+
+  onSubmit() {
+    /* an error from the backend */
+    this.loginError = "Incorrect email or password. Check the entered data";
+    this.disableLoginButton = true; // disable the button while requesting to the server
+
+    // login
+  }
 }
