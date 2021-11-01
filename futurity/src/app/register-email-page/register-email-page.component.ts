4f4feb4580d1fb4e9d68889f8b0e614f1baa1184
ThePersonThat <gaudabrus@inbox.ru>
2021-11-01 20:22:04 +0200

    Added logic to the register page: 	- The email field validation 	- Showing errors

--- futurity/src/app/register-email-page/register-email-page.component.ts
+++ futurity/src/app/register-email-page/register-email-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register-email-page',
@@ -7,6 +7,23 @@ import {FormGroup} from "@angular/forms";
   styleUrls: ['./register-email-page.component.css']
 })
 export class RegisterEmailPageComponent implements OnInit {
-  ngOnInit(): void {
+  emailInputGroup: FormGroup;
+  disableContinueButton = false;
+  emailError: string = null;
+
+
+  ngOnInit() {
+    this.emailInputGroup = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
+      ])
+    });
+  }
+
+  onSubmit() {
+    this.emailError = "This email is already taken";
+    this.disableContinueButton = true; // disable the button while requesting to the server
+    // submit email
   }
 }
