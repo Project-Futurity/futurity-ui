6b8369cf42fd62ec1e785f23063e22a8e38390f8
ThePersonThat <gaudabrus@inbox.ru>
2021-11-01 20:56:00 +0200

    Created the auth service with the email regex

--- futurity/src/app/login-page/login-page.component.ts
+++ futurity/src/app/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../shared/services/auth.service";
 
 @Component({
   selector: 'app-login-page',
@@ -13,11 +14,14 @@ export class LoginPageComponent implements OnInit {
   loginForm: FormGroup;
   loginError: string = null;
 
+  constructor(private auth: AuthService) {
+  }
+
   ngOnInit() {
     this.loginForm = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
-        Validators.pattern("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
+        Validators.pattern(this.auth.EMAIL_REGEX)
       ]),
       password: new FormControl(null, [
         Validators.required,
--- futurity/src/app/register-email-page/register-email-page.component.ts
+++ futurity/src/app/register-email-page/register-email-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../shared/services/auth.service";
 
 @Component({
   selector: 'app-register-email-page',
@@ -11,12 +12,14 @@ export class RegisterEmailPageComponent implements OnInit {
   disableContinueButton = false;
   emailError: string = null;
 
+  constructor(private auth: AuthService) {
+  }
 
   ngOnInit() {
     this.emailInputGroup = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
-        Validators.pattern("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
+        Validators.pattern(this.auth.EMAIL_REGEX)
       ])
     });
   }
