4e60c60a41b783323046a04b9582bc2cc5fced5c
ThePersonThat <gaudabrus@inbox.ru>
2021-11-08 18:38:36 +0200

    Added logic to the register user data page 	- Validation 	- Loading avatar 	- showing/hiding passwords

--- futurity/src/app/register-user-data-page/register-user-data-page.component.ts
+++ futurity/src/app/register-user-data-page/register-user-data-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
+import {EmailService} from "../shared/services/email.service";
 
 @Component({
   selector: 'app-register-user-data-page',
@@ -6,10 +8,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-user-data-page.component.css']
 })
 export class RegisterUserDataPageComponent implements OnInit {
+  avatarUrl: string = null;
+  avatar: File;
+  registerForm: FormGroup;
 
-  constructor() { }
+  showPasswords = false;
+  passwordType = "password"; // text or password
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      nickname: new FormControl("", [
+        Validators.required, Validators.minLength(4)
+      ]),
+      password: new FormControl("", [
+        Validators.required, Validators.minLength(6)
+      ]),
+      confirmPassword: new FormControl("", [Validators.required])
+    }, {validators: this.checkPassword});
   }
 
+  onChangeAvatar(event: any) {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = () => {
+      this.avatarUrl = reader.result as string;
+      this.avatar = event.target.files[0];
+    }
+  }
+
+  getAvatar(): string {
+    return this.avatarUrl == null ? "assets/user.png" : this.avatarUrl;
+  }
+
+  togglePasswordView() {
+    this.showPasswords = !this.showPasswords;
+    this.passwordType = this.showPasswords ? "text" : "password";
+  }
+
+  private checkPassword(form: AbstractControl): ValidationErrors | null {
+    const password = form.get('password').value;
+    const confirmPassword = form.get('confirmPassword').value;
+
+    return password === confirmPassword ? null : {match: true};
+  }
 }
