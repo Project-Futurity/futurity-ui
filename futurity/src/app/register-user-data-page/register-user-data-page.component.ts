45b9867b4a7d038dda18475e846086a8eb879665
ThePersonThat <gaudabrus@inbox.ru>
2021-11-09 21:03:10 +0200

    Separeted the avatar functionality to the avatar service

--- futurity/src/app/register-user-data-page/register-user-data-page.component.ts
+++ futurity/src/app/register-user-data-page/register-user-data-page.component.ts
@@ -1,21 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
-import {EmailService} from "../shared/services/email.service";
+import {AvatarService} from "../shared/services/avatar.service";
 
 @Component({
   selector: 'app-register-user-data-page',
   templateUrl: './register-user-data-page.component.html',
-  styleUrls: ['./register-user-data-page.component.css']
+  styleUrls: ['./register-user-data-page.component.css'],
+  providers: [AvatarService]
 })
 export class RegisterUserDataPageComponent implements OnInit {
-  avatarUrl: string = null;
-  avatar: File;
   registerForm: FormGroup;
-
   showPasswords = false;
   passwordType = "password"; // text or password
 
-  constructor() {}
+  constructor(private avatarService: AvatarService) {}
 
   ngOnInit(): void {
     this.registerForm = new FormGroup({
@@ -30,17 +28,11 @@ export class RegisterUserDataPageComponent implements OnInit {
   }
 
   onChangeAvatar(event: any) {
-    const reader = new FileReader();
-
-    reader.readAsDataURL(event.target.files[0]);
-    reader.onload = () => {
-      this.avatarUrl = reader.result as string;
-      this.avatar = event.target.files[0];
-    }
+    this.avatarService.loadImage(event.target.files[0]);
   }
 
   getAvatar(): string {
-    return this.avatarUrl == null ? "assets/user.png" : this.avatarUrl;
+    return this.avatarService.getAvatar();
   }
 
   togglePasswordView() {
@@ -52,6 +44,6 @@ export class RegisterUserDataPageComponent implements OnInit {
     const password = form.get('password').value;
     const confirmPassword = form.get('confirmPassword').value;
 
-    return password === confirmPassword ? null : {match: true};
+    return password === confirmPassword ? null : {notMatch: true};
   }
 }
---
+++ futurity/src/app/shared/services/avatar.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AvatarService {
+  private readonly DEFAULT_IMAGE_URL = "/assets/user.png";
+  private readonly ALLOWED_EXTENSIONS = ['jpeg', 'png', 'gif'];
+  private avatar: File;
+  private avatarUrl: string;
+
+  constructor() { }
+
+  loadImage(image: File): void {
+    if (!this.isCorrectExtension(image.type)) {
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload = () => {
+      this.avatarUrl = reader.result as string;
+      this.avatar = image;
+    };
+  }
+
+  getAvatar() {
+    return this.avatarUrl == null ? this.DEFAULT_IMAGE_URL : this.avatarUrl;
+  }
+
+  private isCorrectExtension(type: string): boolean {
+    const ex = type.split("/")[1];
+
+    return this.ALLOWED_EXTENSIONS.includes(ex);
+  }
+}
