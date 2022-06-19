ed36b3bb97dcbb51ccbf7f96327cfed0e9fae88f
ThePersonThat <gaudabrus@inbox.ru>
2022-06-19 16:45:23 +0300

    Alert-Popup:  - Fixed css

--- futurity/src/app/alert-popup/alert-popup.component.ts
+++ futurity/src/app/alert-popup/alert-popup.component.ts
@@ -1,16 +1,21 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Inject, Input, OnInit} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-alert-popup',
   templateUrl: './alert-popup.component.html',
   styleUrls: ['./alert-popup.component.css']
 })
-export class AlertPopupComponent implements OnInit {
+export class AlertPopupComponent implements AfterViewInit {
   @Input() message: string;
 
-  constructor(public activeModal: NgbActiveModal) { }
+  constructor(public activeModal: NgbActiveModal, @Inject(DOCUMENT) private document: Document) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    const content = this.document.querySelector(".modal-content") as any;
+
+    content.style.borderRadius = "25px";
+    content.style.background = "#282c35";
   }
 }
